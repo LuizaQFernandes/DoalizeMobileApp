53871f1153de26151d278ff494db2eef5ffaa0e3
LuizaQFernandes <luhqfernandes@gmail.com>
2022-05-21 19:27:05 -0300

    first commit

---
+++ src/themes/padrao.js
@@ -0,0 +1,11 @@
+export default {
+    colors: {
+        rosa: '#E40037', azul: '#2BB0BE', amarelo: '#FFD632', preto: '#282828', branco: '#FCFCFC'
+    },
+    fonts: {
+        roboto:{
+            regular: 'Roboto_400Regular', bold:'Roboto_700Bold'
+        },
+
+    }
+}
